c8abfae1cf86cda7e3d9698d42973ed093d4ca11
jeongjaein <wodls96@gmail.com>
2020-10-20 16:43:02 +0900

    feat : create MannaListProtocols

---
+++ socketRocket/MannaList/MannaListProtocols.swift
@@ -0,0 +1,21 @@
+//
+//  MannaListProtocols.swift
+//  socketRocket
+//
+//  Created by 정재인 on 2020/10/20.
+//
+
+import UIKit
+
+
+protocol MannaListViewProtocol {
+    
+}
+
+protocol MannaListViewModelProtocol {
+    
+}
+
+protocol MannaListModelProtocol {
+    
+}
